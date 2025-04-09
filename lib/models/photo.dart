3b9abc191874c4bdfedb8ee3e9f3f925071c3aa7
rubenk <ruben.k@captainfresh.in>
2025-04-09 09:05:37 +0530

    photo model and repo to access from api and store in db

---
+++ lib/models/photo.dart
@@ -0,0 +1,35 @@
+class Photo {
+  final int id;
+  final int albumId;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  Photo({
+    required this.id,
+    required this.albumId,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      id: json['id'],
+      albumId: json['albumId'],
+      title: json['title'],
+      url: json['url'],
+      thumbnailUrl: json['thumbnailUrl'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'albumId': albumId,
+      'title': title,
+      'url': url,
+      'thumbnailUrl': thumbnailUrl,
+    };
+  }
+}
---
+++ lib/repositories/photo_repository.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+import '../models/photo.dart';
+import '../database_service.dart';
+
+class PhotoRepository {
+  final DatabaseService _databaseService;
+  final Dio _dio;
+  static const String _baseUrl = 'https://jsonplaceholder.typicode.com';
+
+  PhotoRepository(this._databaseService)
+      : _dio = Dio(BaseOptions(baseUrl: _baseUrl));
+
+  Future<List<Photo>> fetchAndStorePhotos() async {
+    try {
+      // Fetch photos from API
+      final response = await _dio.get('/photos');
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonList = response.data;
+        final List<Photo> photos =
+            jsonList.map((json) => Photo.fromJson(json)).toList();
+
+        // Store photos in local database
+        await _databaseService.insertPhotos(photos);
+
+        return photos;
+      } else {
+        throw Exception('Failed to load photos');
+      }
+    } on DioException catch (e) {
+      throw Exception('Error fetching photos: ${e.message}');
+    } catch (e) {
+      throw Exception('Error fetching photos: $e');
+    }
+  }
+
+  Future<List<Photo>> getLocalPhotos() async {
+    return await _databaseService.getAllPhotos();
+  }
+
+  Future<void> clearLocalPhotos() async {
+    await _databaseService.deleteAllPhotos();
+  }
+}
